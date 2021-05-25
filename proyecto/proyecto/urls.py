7ef5f61a18768c3e3eba67c740d235b4aa9a8316
VicenteUcn <vicente.figueroa@alumnos.ucn.cl>
2021-05-24 23:03:23 -0400

    primer template
    manejo del primer template django

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from cesfam import views
 from django.contrib import admin
 from django.urls import path
-from proyecto.views import bienvenido
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',bienvenido)
+    path('', views.index)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
